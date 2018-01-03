7e43825cc499c62f14e885ff130c13005eff04e1
Jake Palmer <jake.michael96@gmail.com>
2018-01-03 02:03:56 -0800

    initial upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function() {
+	Typed.new("#typed", {
+		strings: ["I build websites.", "I engineer Applications.", "I develop software.", "I study Computer Science."],
+		startDelay: 500,
+		backDelay: 2000,
+		contentType: 'text',
+		backSpeed: 30,
+		typeSpeed: 40,
+		loop: null
+	});
+});
